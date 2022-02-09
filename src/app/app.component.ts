243084eb53bb8c90e27cf0d8db74a433ede9ab54
Alex <alex.fiendish@gmail.com>
2022-02-09 12:00:26 +0200

    added components overview, lifecycle hooks overview

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  posts: Post[] = [
+    {
+      title: 'Want to learn Angular components',
+      text: 'I still learning components',
+      id: 1,
+    },
+    { title: 'Next block', text: 'Will be about directives and pipes', id: 2 },
+  ];
+
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   console.log('timeout');
+    //   this.posts[0] = {
+    //     title: 'changed',
+    //     text: 'changed 2',
+    //     id: 33,
+    //   };
+    // }, 5000);
+  }
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+    console.log('Post', post);
+  }
+
+  removePost(id: number) {
+    console.log('Id to remove', id);
+    this.posts = this.posts.filter((post) => post.id !== id);
+  }
+}
